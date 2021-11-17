1fabe781222152935e359975d3738c275866dd31
myklegaspi1218 <myklegaspiwtw@icloud.com>
2021-11-17 19:50:28 +0800

    wsgi procfile

--- hcbresourcesite/hcbcapacity/urls.py
+++ hcbresourcesite/hcbcapacity/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib import admin
 
 from . import views
 
 app_name = 'hcbcapacity'
 
 urlpatterns = [
+    path(r'', admin.site.urls),
     path(r'', views.index, name='index'),
     path(r'^update-api-table/', views.update_api_table, name='update-api-table' ),
     path(r'^pie-chart/', views.pie_chart, name='pie-chart'),
