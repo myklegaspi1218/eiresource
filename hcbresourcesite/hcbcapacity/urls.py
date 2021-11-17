f4f2ae5d607bbc199520f60e98475ccf1ba9a4b1
myklegaspi1218 <myklegaspiwtw@icloud.com>
2021-11-17 19:39:57 +0800

    wsgi procfile

--- hcbresourcesite/hcbcapacity/urls.py
+++ hcbresourcesite/hcbcapacity/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = 'hcbcapacity'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('update-api-table/', views.update_api_table, name='update-api-table' ),
-    path('pie-chart/', views.pie_chart, name='pie-chart'),
-    path('proj-wo-da/', views.proj_without_da, name='project-wo-da'),
+    path(r'', views.index, name='index'),
+    path(r'^update-api-table/', views.update_api_table, name='update-api-table' ),
+    path(r'^pie-chart/', views.pie_chart, name='pie-chart'),
+    path(r'^proj-wo-da/', views.proj_without_da, name='project-wo-da'),
     path('<int:prod_id>/', views.daProjects, name='da-projects'),
-    path('enter-info/', views.enter_info,name='enter-info'),
+    path(r'^enter-info/', views.enter_info,name='enter-info'),
    
 ]
 
