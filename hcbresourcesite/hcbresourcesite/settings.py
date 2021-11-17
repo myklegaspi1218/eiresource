4d0dcdb4aae505e48ab5cf45fc96a4e5a1f9f532
myklegaspi1218 <myklegaspiwtw@icloud.com>
2021-11-17 20:16:55 +0800

    wsgi procfile

--- hcbresourcesite/hcbresourcesite/settings.py
+++ hcbresourcesite/hcbresourcesite/settings.py
@@ -135,6 +135,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
