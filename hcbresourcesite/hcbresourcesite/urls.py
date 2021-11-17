b19bcef599c265e490337dc123b05affeebcdd4a
myklegaspi1218 <myklegaspiwtw@icloud.com>
2021-11-17 21:29:14 +0800

    wsgi procfile

--- hcbresourcesite/hcbresourcesite/urls.py
+++ hcbresourcesite/hcbresourcesite/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path(r'^',include('hcbresourcesite.urls')),
+    path(r'',include('hcbresourcesite.urls')),
     
 ]
